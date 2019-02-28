6dc9173704e24942181b1bc4ff5a0da7fbad838d
Kyle.Howard <k.howard@wearedoubleline.com>
2019-02-28 15:41:23 -0600

    Update 4 still playing

--- HDShoutSpace/SlackTest2.ps1
+++ HDShoutSpace/SlackTest2.ps1
@@ -1,6 +1,4 @@
-﻿$TOKEN = 'xoxp-562955663906-563280409845-562438496752-a508e9bf4b7672770a952100100fac80'
-
-New-SlackMessageAttachment -Color $([System.Drawing.Color]::red) `
+﻿New-SlackMessageAttachment -Color $([System.Drawing.Color]::red) `
                            -Title 'The System Is Down' `
                            -TitleLink https://www.youtube.com/watch?v=TmpRs7xN06Q `
                            -Text 'Please Do The Needful' `
@@ -10,4 +8,4 @@ New-SlackMessageAttachment -Color $([System.Drawing.Color]::red) `
                            -Fallback 'Your client is bad' |
     New-SlackMessage -Channel 'test' `
                      -IconEmoji :bomb: |
-    Send-SlackMessage -TOKEN $TOKEN
+    Send-SlackMessage -TOKEN '$Encrypted'
