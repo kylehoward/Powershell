60b8afe96884345ee46d98b48a1029edb27c29ce
Kyle.Howard <k.howard@wearedoubleline.com>
2019-02-28 15:41:23 -0600

    Update 4 still playing

---
+++ HDShoutSpace/driveenumerationtest.ps1
@@ -0,0 +1,9 @@
+﻿#!/usr/bin/env powershell
+$cptr = [System.Net.Dns]::GetHostName()
+write-host $cptr
+
+$disk = ([wmi]"\\$cptr\root\cimv2:Win32_logicalDisk.DeviceID='c:'")
+
+"$_(cptr) C: has {0:#.0} GB free of {1:#.0} GB Total" -f ($disk.FreeSpace/1GB),($disk.Size/1GB) | write-output
+
+write-host {"\\$cptr\root\cimv2:Win32_logicalDisk.DeviceID='c:'"}
