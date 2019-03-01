39cc5ba8375c5b62701515fdc415ba91167e47e9
Kyle.Howard <k.howard@wearedoubleline.com>
2019-02-28 23:58:47 -0600

    --- Auto-Git Commit ---

---
+++ diskspace2.ps1
@@ -0,0 +1,2 @@
+﻿gwmi Win32_LogicalDisk -Filter "DeviceID='C:'" | select Name, FileSystem,FreeSpace,BlockSize,Size | % {$_.BlockSize=(($_.FreeSpace)/($_.Size))*100;$_.FreeSpace=($_.FreeSpace/1GB);$_.Size=($_.Size/1GB);$_}| Format-Table Name, @{n='FS';e={$_.FileSystem}},@{n='Free, Gb';e={'{0:N2}'-f
+$_.FreeSpace}}, @{n='Free,%';e={'{0:N2}'-f $_.BlockSize}} -AutoSize
