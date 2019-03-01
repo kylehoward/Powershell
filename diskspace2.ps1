63c3c1c810b215afd3acdc212eef3d0f55a3e78d
Kyle.Howard <k.howard@wearedoubleline.com>
2019-03-01 11:17:17 -0600

    --- Auto-Git Commit ---

--- diskspace2.ps1
+++ diskspace2.ps1
@@ -1,2 +1,17 @@
-﻿gwmi Win32_LogicalDisk -Filter "DeviceID='C:'" | select Name, FileSystem,FreeSpace,BlockSize,Size | % {$_.BlockSize=(($_.FreeSpace)/($_.Size))*100;$_.FreeSpace=($_.FreeSpace/1GB);$_.Size=($_.Size/1GB);$_}| Format-Table Name, @{n='FS';e={$_.FileSystem}},@{n='Free, Gb';e={'{0:N2}'-f
+﻿
+function Select-Something
+{
+  param
+  (
+    [Object]
+    [Parameter(Mandatory=$true, ValueFromPipeline=$true, HelpMessage="Data to process")]
+    $InputObject
+  )
+  process
+  {
+    $InputObject.BlockSize=(($InputObject.FreeSpace)/($InputObject.Size))*100;$InputObject.FreeSpace=($InputObject.FreeSpace/1GB);$InputObject.Size=($InputObject.Size/1GB);$InputObject
+  }
+}
+
+gwmi Win32_LogicalDisk -Filter "DeviceID='C:'" | select Name, FileSystem,FreeSpace,BlockSize,Size | Select-Something| Format-Table Name, {n='FS';e={$_.FileSystem}},@{n='Free, Gb';e={'{0:N2}'-f
 $_.FreeSpace}}, @{n='Free,%';e={'{0:N2}'-f $_.BlockSize}} -AutoSize
---
+++ diskspace3.ps1
@@ -0,0 +1,12 @@
+﻿{
+    $CDisk = GWMI Win32_LogicalDisk -Filter "DeviceID='C:'"
+    $CDisk = @{'Size'      = [Math]::Round($CDisk.Size / 1GB);
+               'FreeSpace' = [Math]::Round($CDisk.FreeSpace / 1GB)}
+    $CDisk.Add('Usage', ($CDisk.Size - $CDisk.FreeSpace))
+    $CDisk.Add('PercentUsage', [Math]::Round(($CDisk.Usage / $CDisk.Size) * 100))
+
+    "C: drive free space: $($CDisk.FreeSpace)GB"
+    "C: drive capacity:   $($CDisk.Size)GB"
+    '--------------------------------'
+    "Disk usage:          $($CDisk.Usage)GB ($($CDisk.PercentUsage)%)"
+}
