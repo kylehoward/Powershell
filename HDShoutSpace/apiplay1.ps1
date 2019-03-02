f8101d9c44c0cd8cc84159118b0258c7b445a5b7
Kyle.Howard <k.howard@wearedoubleline.com>
2019-03-02 13:34:41 -0600

    --- Auto-Git Commit ---

---
+++ HDShoutSpace/apiplay1.ps1
@@ -0,0 +1,4 @@
+﻿$url = "http://srv.local/api/login" +
+    "?order=desc&sort=activity&tagged=powershell&pagesize=10&site=stackoverflow"
+$result = Invoke-RestMethod $url
+$result.Items | Foreach-Object { $_.Title; $_.Link; "" }
