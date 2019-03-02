0c51c6e5d8ecb8cf596b28d0d6578bb598be2aac
Kyle.Howard <k.howard@wearedoubleline.com>
2019-03-02 12:34:29 -0600

    --- Auto-Git Commit ---

---
+++ jiraonnect`.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module -Name JiraPS
+
+Set-JiraConfigServer -Server 'https://kyle-howard.atlassian.net'
+
+$cred = Get-Credential -UserName 'Alert'
+
+#Get-JiraIssue -Key 'inc-1' -Credential $cred
+
+New-JiraSession -Credential $cred
