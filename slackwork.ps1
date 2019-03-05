0be7e56c5f1803c439d195bb2f92c8fd14b801b4
Kyle.Howard <k.howard@wearedoubleline.com>
2019-03-05 12:58:11 -0600

    playing with slack oath

---
+++ slackwork.ps1
@@ -0,0 +1,11 @@
+﻿New-SlackMessageAttachment -Color $([System.Drawing.Color]::red) `
+                           -Title 'The System Is Down' `
+                           -TitleLink https://www.youtube.com/watch?v=TmpRs7xN06Q `
+                           -Text 'Please Do The Needful' `
+                           -Pretext 'Everything is broken' `
+                           -AuthorName 'SCOM Bot' `
+                           -AuthorIcon 'http://ramblingcookiemonster.github.io/images/tools/wrench.png' `
+                           -Fallback 'Your client is bad' |
+    New-SlackMessage -Channel 'test' `
+                     -IconEmoji :bomb: |
+    Send-SlackMessage -TOKEN 'xoxp-562955663906-563280409845-563511504369-ce957def80225185c0780910c6cccc51'
